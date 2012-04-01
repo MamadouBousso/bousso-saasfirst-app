43471294591f7a1d8505ef854a59b833df70ff75
Mamadou Bousso <mbousso@univ-thies.sn>
2012-04-01 07:20:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoussoSaasfirstApp::Application.config.secret_token = '4b6f77b24d6dd3690d02503b18d45dea8b4938e91075a5fb473eda72378e7560929bbb4077cd315d0ee0383c50580a933ae7aa598631b2cd2aaf295572d97611'
